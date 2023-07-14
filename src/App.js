fd42e1d0d91b2cbebd31e1706324e391794a8bf4
Richard Young <richardyoung@Richards-MacBook-Air.local>
2023-07-14 19:35:26 -0400

    Added NavBar, Hero, and Featured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-// import NavBar from './components/NavBar';
-import NavBar from './components/NavBar';
+// import NavBar from './components/navbar/NavBar'
+import NavBar from './components/navbar/NavBar';
+import Hero from './components/hero/Hero';
+import Best from "./components/best/Best"
+import Featured from "./components/featured/Featured"
 
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
+      <Best />
+      <Featured />
     </>
   );
 }
